b17ead0044ba95a1d6ab9cd03177ce65423059ec
damirsulejmanov <106074225+damirsulejmanov@users.noreply.github.com>
2022-05-25 17:15:56 +0700

    Start

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string privet = "привет"; // привет
+            string howAreYou = "как дела";
+
+            string fine = "нормально"; // как дела
+
+            string question = "";
+            string whatIsYourName = "как тебя зовут";
+            string poka = "пока";
+            string ne ponimayu = "я не понимаю";
+            while (question != poka)
+            {
+                question = Console.ReadLine();
+                if (question == privet)
+                {
+                    Console.WriteLine(privet);
+                }
+                else if (question == howAreYou)
+                {
+                    Console.WriteLine(howAreYou);
+                }
+                else if (question == fine)
+                {
+                    Console.WriteLine(fine);
+                }
+                else if (question == whatIsYourName)
+                {
+                    Console.WriteLine("Настя");
+                }
+                else if (question == poka)
+                {
+                    Console.WriteLine(poka);
+                }
+            }
+
+            
+
+        }
+    }
+}
